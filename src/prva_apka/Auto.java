3569d933bf39759189e2494fa25992f3e201d6ba
Matej <matej.kellner@gmail.com>
2017-08-28 14:57:07 +0200

    prvy commit

---
+++ src/prva_apka/Auto.java
@@ -0,0 +1,18 @@
+package prva_apka;
+
+public class Auto {
+
+    private String spz;
+
+
+    public void setSpz(String spz) {
+        this.spz = spz;
+    }
+
+    public String getSpz() {
+        return spz;
+
+    }
+
+
+}
---
+++ src/prva_apka/Main.java
@@ -0,0 +1,19 @@
+package prva_apka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("jede jede masinka!!! ");
+
+        Auto a = new Auto();
+        a.setSpz("BA-284-ZU");
+        Auto b = new Auto();
+        b.setSpz("MA-291-DP");
+        Auto c = new Auto();
+        c.setSpz("PP-007-KK");
+
+        System.out.println(a.getSpz());
+
+    }
+}
